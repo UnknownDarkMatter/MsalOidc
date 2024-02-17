08e619e864e12b92573b18d5d621261670cb1e4e
Sebastien BONELLI <ladebrouille4587@gmail.com>
2024-02-17 01:30:53 +0100

    WIP : login

--- src/MsalOidc/client-app/src/app/infrastructure/account-service/account-service.component.ts
+++ src/MsalOidc/client-app/src/app/infrastructure/account-service/account-service.component.ts
@@ -1,16 +1,98 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MsalBroadcastService, MsalModule, MsalService } from '@azure/msal-angular';
-import { AuthenticationResult, EventMessage, EventType, InteractionStatus } from '@azure/msal-browser';
+import { AuthenticationResult, EventMessage, EventType, InteractionStatus} from '@azure/msal-browser';
 import { filter } from 'rxjs/operators';
+
+import { MSAL_GUARD_CONFIG, MsalGuardConfiguration } from '@azure/msal-angular';
+import { PopupRequest, RedirectRequest, LogLevel  } from '@azure/msal-browser';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { IPublicClientApplication, PublicClientApplication, InteractionType, BrowserCacheLocation } from '@azure/msal-browser';
+import { MsalGuard, MsalInterceptor, MsalInterceptorConfiguration, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalRedirectComponent } from '@azure/msal-angular';
+import { environment } from '../../../environments/environment';
+
+export function loggerCallback(logLevel: LogLevel, message: string) {
+  console.log(message);
+}
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: environment.msalConfig.auth.clientId,
+      authority: environment.msalConfig.auth.authority,
+      redirectUri: '/',
+      postLogoutRedirectUri: '/'
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage
+    },
+    system: {
+      allowNativeBroker: false, // Disables WAM Broker
+      loggerOptions: {
+        loggerCallback,
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false
+      }
+    }
+  });
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set(environment.apiConfig.uri, environment.apiConfig.scopes);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap
+  };
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return { 
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: [...environment.apiConfig.scopes]
+    },
+    loginFailedRoute: '/login-failed'
+  };
+}
+
+
 
 @Component({
   selector: 'app-account-service',
   standalone: true,
-  imports: [HttpClientModule,
-    MsalModule],
   templateUrl: './account-service.component.html',
-  styleUrl: './account-service.component.scss'
+  styleUrl: './account-service.component.scss',
+  imports: [
+    HttpClientModule,
+    MsalModule
+  ],
+  providers:[
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService
+  ],
 })
 export class AccountServiceComponent implements OnInit {
   loginDisplay = false;
