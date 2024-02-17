2670560b10ccad312c797142a57f53f5f6d66c7e
Sebastien BONELLI <ladebrouille4587@gmail.com>
2024-02-17 18:13:02 +0100

    configure JwtBearer at backend

--- src/MsalOidc.Entities/AzureAdConfiguration.cs
+++ src/MsalOidc.Entities/AzureAdConfiguration.cs
@@ -8,10 +8,7 @@ namespace OicdDemo.Entities;
 
 public class AzureAdConfiguration
 {
-    public string ClientId { get; set; }
-    public string ClientSecret { get; set; }
     public string Audience { get; set; }
     public string Authority { get; set; }
-    public string ReturnUrl { get; set; }
-    public string MetadataAddress { get; set; }
+    public string Issuer { get; set; }
 }
--- src/MsalOidc/Program.cs
+++ src/MsalOidc/Program.cs
@@ -1,45 +1,42 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication;
 using System.Security.Claims;
 using OicdDemo.Entities;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 var azureSettings = builder.Configuration.GetSection("AzureAd").Get<AzureAdConfiguration>();
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
     {
-        options.Cookie.SameSite = SameSiteMode.None; 
-        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-    })
-    .AddOpenIdConnect(options =>
-    {
-        options.Authority = $"{azureSettings.Authority}";
-        options.ClientId = azureSettings.ClientId;
-        options.ClientSecret = azureSettings.ClientSecret;
-        options.MetadataAddress = azureSettings.MetadataAddress;
-
-        options.ResponseType = "code";
-        options.SaveTokens = true;
-
-        //options.CallbackPath = new PathString("/"); // Needs to be registered as callback url by identity provider
-        options.Scope.Clear();
-        options.Scope.Add("openid");
-        options.Scope.Add("profile");
-        //options.Scope.Add("email");
-        //options.Scope.Add("offline_access"); // Required to get a refresh_token // https://learn.microsoft.com/en-us/azure/active-directory/develop/scopes-oidc#offline_access
-
-        //options.GetClaimsFromUserInfoEndpoint = true;
-
-        //options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-        //options.ClaimActions.MapJsonKey(ClaimTypes.GivenName, "given_name");
-        //options.ClaimActions.MapJsonKey(ClaimTypes.Surname, "family_name");
+        options.Authority = azureSettings.Authority;
+        options.Audience = azureSettings.Audience;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = azureSettings.Issuer,
+            ValidAudience = azureSettings.Audience
+        };
     });
 
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(options =>
+{
+    options.DefaultPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
 
 
 // Add services to the container.
